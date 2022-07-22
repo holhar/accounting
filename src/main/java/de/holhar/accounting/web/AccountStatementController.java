d2977a4de6a8c628b8f5bbc441356099295fef27
Holger Harms <holhar@posteo.de>
2022-07-22 19:02:23 +0200

    Implement AccountStatementController

---
+++ src/main/java/de/holhar/accounting/web/AccountStatementController.java
@@ -0,0 +1,51 @@
+package de.holhar.accounting.web;
+
+import de.holhar.accounting.domain.MonthlyReportRepository;
+import de.holhar.accounting.service.AccountStatementService;
+import de.holhar.accounting.service.CsvDeSanitizationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/statements")
+public class AccountStatementController {
+
+  private static final Logger logger = LoggerFactory.getLogger(AccountStatementController.class);
+
+  private final AccountStatementService accountStatementService;
+  private final CsvDeSanitizationService csvDeSanitizationService;
+
+  public AccountStatementController(
+      AccountStatementService accountStatementService,
+      CsvDeSanitizationService csvDeSanitizationService
+  ) {
+    this.accountStatementService = accountStatementService;
+    this.csvDeSanitizationService = csvDeSanitizationService;
+  }
+
+  @PostMapping("/import")
+  public ResponseEntity<String> batchImport() {
+    try {
+      accountStatementService.importStatements();
+    } catch (Exception e) {
+      logger.error("Reports import failed", e);
+      return ResponseEntity.internalServerError().body("Batch import failed: " + e.getMessage());
+    }
+    return ResponseEntity.ok("Batch import successful");
+  }
+
+  @PostMapping("/de-sanitize")
+  public ResponseEntity<String> migrateType() {
+    try {
+      csvDeSanitizationService.deSanitize();
+    } catch (Exception e) {
+      logger.error("Type migration failed", e);
+      return ResponseEntity.internalServerError().body("Type migration failed: " + e.getMessage());
+    }
+    return ResponseEntity.ok("Type migration successful");
+  }
+}

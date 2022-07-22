b1ba9ed5cf3de95aa80b08a15ab8059ca436d809
Holger Harms <holhar@posteo.de>
2022-07-22 18:49:05 +0200

    Remove CliAdapter

--- src/main/java/de/holhar/accounting/AccountingApplication.java
+++ src/main/java/de/holhar/accounting/AccountingApplication.java
@@ -1,9 +1,6 @@
 package de.holhar.accounting;
 
-import de.holhar.accounting.adapter.CliAdapter;
 import de.holhar.accounting.config.AppProperties;
-import java.util.Optional;
-import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,18 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties({AppProperties.class})
 public class AccountingApplication {
 
-  private final Optional<CliAdapter> cliAdapter;
-
-  public AccountingApplication(Optional<CliAdapter> cliAdapter) {
-    this.cliAdapter = cliAdapter;
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(AccountingApplication.class, args);
-  }
-
-  @PostConstruct
-  private void init() {
-    cliAdapter.ifPresent(CliAdapter::startApplication);
   }
 }

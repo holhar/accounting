3ab3f29d6c9f342f07d270ff469e7059516b6645
Holger Harms <holhar@posteo.de>
2022-09-26 22:23:17 +0200

    Add Money module to Jackson ObjectMapper bean.

--- src/main/java/de/holhar/accounting/AccountingConfiguration.java
+++ src/main/java/de/holhar/accounting/AccountingConfiguration.java
@@ -1,5 +1,9 @@
 package de.holhar.accounting;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import de.holhar.accounting.report.adapter.in.web.FileHandler;
 import de.holhar.accounting.report.application.port.out.LoadStatementsPort;
 import de.holhar.accounting.report.application.service.deserialization.CheckingAccountEntryDeserializer;
@@ -9,9 +13,12 @@ import de.holhar.accounting.report.application.service.deserialization.Deseriali
 import de.holhar.accounting.report.application.service.report.AccountReportManager;
 import de.holhar.accounting.report.application.service.report.ReportCalculator;
 import de.holhar.accounting.report.application.service.sanitation.FileSanitizer;
+import java.text.SimpleDateFormat;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.zalando.jackson.datatype.money.MoneyModule;
 
 @Configuration
 @EnableConfigurationProperties({AccountingConfigurationProperties.class})
@@ -62,5 +69,17 @@ public class AccountingConfiguration {
         accountStatementDeserializer,
         creditCardStatementDeserializer
     );
+  }
+
+  @Bean
+  @Primary
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper()
+        .registerModule(new MoneyModule().withAmountFieldName("majorUnit"))
+        .findAndRegisterModules()
+        .setSerializationInclusion(Include.NON_EMPTY)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
   }
 }

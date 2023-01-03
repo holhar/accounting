580ecd585d7972666cf0260547926a1f4484cbf4
Holger Harms <holhar@posteo.de>
2023-01-03 19:14:01 +0100

    Comment in and fix CreditCardEntryDeserializerTest and CheckingAccountEntryDeserializerTest.

--- src/test/java/de/holhar/accounting/report/application/service/deserialization/CheckingAccountEntryDeserializerTest.java
+++ src/test/java/de/holhar/accounting/report/application/service/deserialization/CheckingAccountEntryDeserializerTest.java
@@ -3,62 +3,70 @@ package de.holhar.accounting.report.application.service.deserialization;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import de.holhar.accounting.common.MoneyUtils;
+import de.holhar.accounting.report.domain.CheckingAccountEntry;
+import de.holhar.accounting.report.domain.Entry;
+import de.holhar.accounting.report.domain.EntryType;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 class CheckingAccountEntryDeserializerTest {
 
-//  @Test
-//  void readStatement() throws IOException {
-//    // Given
-//    CheckingAccountEntryDeserializer deserializer = new CheckingAccountEntryDeserializer();
-//    Path processedFile = Paths.get("src/test/resources/accounting/sanitized/acc_202001.csv");
-//    List<String> lines = Files.readAllLines(processedFile);
-//
-//    // When
-//    AccountStatement accountStatement = deserializer.readStatement(lines);
-//
-//    // Then
-//    assertEquals(AccountStatement.Type.CHECKING_ACCOUNT, accountStatement.getType());
-//    assertEquals("XX01234567890123456789", accountStatement.getId());
-//    assertEquals(LocalDate.parse("2020-01-01"), accountStatement.getFrom());
-//    assertEquals(LocalDate.parse("2020-01-31"), accountStatement.getTo());
-//    assertEquals(new BigDecimal("1234.56"), accountStatement.getBalance().getValue());
-//    assertEquals(LocalDate.parse("2020-01-31"), accountStatement.getBalance().getDate());
-//    assertEquals(31, accountStatement.getEntries().size());
-//
-//    CheckingAccountEntry firstEntry = (CheckingAccountEntry) accountStatement.getEntries().get(0);
-//    assertEquals(LocalDate.parse("2020-01-31"), firstEntry.getBookingDate());
-//    assertEquals(LocalDate.parse("2020-01-31"), firstEntry.getValueDate());
-//    assertEquals("Überweisung", firstEntry.getBookingText());
-//    assertEquals("XXX VISACARD", firstEntry.getClient());
-//    assertEquals(
-//        "9876543210987645 08.39 PETER LUSTIG XXX INTERNET BANKING DATUM 31.01.2020, 08.39 UHR",
-//        firstEntry.getIntendedUse());
-//    assertEquals("XX98765432109876543210", firstEntry.getAccountId());
-//    assertEquals("YYXXCCV9999", firstEntry.getBankCode());
-//    assertEquals(new BigDecimal("-100.00"), firstEntry.getAmount());
-//    assertTrue(firstEntry.getCreditorId().isBlank());
-//    assertTrue(firstEntry.getClientReference().isBlank());
-//    assertEquals("NOTPROVIDED", firstEntry.getCustomerReference());
-//    assertEquals(EntryType.INNER_ACCOUNT_TRANSFER, firstEntry.getType());
-//
-//    CheckingAccountEntry lastEntry = (CheckingAccountEntry) accountStatement.getEntries().get(30);
-//    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getBookingDate());
-//    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getValueDate());
-//    assertEquals("Lastschrift", lastEntry.getBookingText());
-//    assertEquals("Go Aroung Come Around (GACA)", lastEntry.getClient());
-//    assertEquals(
-//        "/RFB/P0000000000/0001, 02.01.2222,42Sub Rate PremiumSubscription 68482646 XXYYX+Blubber Enterprises",
-//        lastEntry.getIntendedUse());
-//    assertEquals("TR33333333333333333335", lastEntry.getAccountId());
-//    assertEquals("BELA66600000", lastEntry.getBankCode());
-//    assertEquals(new BigDecimal("-63.42"), lastEntry.getAmount());
-//    assertEquals("DA55555555557777778", lastEntry.getCreditorId());
-//    assertEquals("0001-900000000000", lastEntry.getClientReference());
-//    assertTrue(lastEntry.getCustomerReference().isBlank());
-//    assertEquals(EntryType.FOOD_AND_DRUGSTORE, lastEntry.getType());
-//  }
+  @Test
+  void readStatement() throws IOException {
+    // Given
+    CheckingAccountEntryDeserializer deserializer = new CheckingAccountEntryDeserializer();
+    Path processedFile = Paths.get("src/test/resources/accounting/sanitized/acc_202001.csv");
+    List<String> lines = Files.readAllLines(processedFile);
+
+    // When
+    Stream<Entry> entryStream = deserializer.readStatement(lines);
+
+    // Then
+    List<Entry> entryList = entryStream.collect(Collectors.toList());
+    assertEquals(31, entryList.size());
+
+    CheckingAccountEntry firstEntry = (CheckingAccountEntry) entryList.get(0);
+    assertEquals(LocalDate.parse("2020-01-31"), firstEntry.getBookingDate());
+    assertEquals(LocalDate.parse("2020-01-31"), firstEntry.getValueDate());
+    assertEquals("Überweisung", firstEntry.getBookingText());
+    assertEquals("XXX VISACARD", firstEntry.getClient());
+    assertEquals(
+        "9876543210987645 08.39 PETER LUSTIG XXX INTERNET BANKING DATUM 31.01.2020, 08.39 UHR",
+        firstEntry.getIntendedUse());
+    assertEquals("XX98765432109876543210", firstEntry.getAccountId());
+    assertEquals("YYXXCCV9999", firstEntry.getBankCode());
+    assertEquals(MoneyUtils.ofMinor(-10000L), firstEntry.getAmount());
+    assertTrue(firstEntry.getCreditorId().isBlank());
+    assertTrue(firstEntry.getClientReference().isBlank());
+    assertEquals("NOTPROVIDED", firstEntry.getCustomerReference());
+    assertEquals(EntryType.INNER_ACCOUNT_TRANSFER, firstEntry.getType());
+
+    CheckingAccountEntry lastEntry = (CheckingAccountEntry) entryList.get(30);
+    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getBookingDate());
+    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getValueDate());
+    assertEquals("Lastschrift", lastEntry.getBookingText());
+    assertEquals("Go Aroung Come Around (GACA)", lastEntry.getClient());
+    assertEquals(
+        "/RFB/P0000000000/0001, 02.01.2222,42Sub Rate PremiumSubscription 68482646 XXYYX+Blubber Enterprises",
+        lastEntry.getIntendedUse());
+    assertEquals("TR33333333333333333335", lastEntry.getAccountId());
+    assertEquals("BELA66600000", lastEntry.getBankCode());
+    assertEquals(MoneyUtils.ofMinor(-6342L), lastEntry.getAmount());
+    assertEquals("DA55555555557777778", lastEntry.getCreditorId());
+    assertEquals("0001-900000000000", lastEntry.getClientReference());
+    assertTrue(lastEntry.getCustomerReference().isBlank());
+    assertEquals(EntryType.FOOD_AND_DRUGSTORE, lastEntry.getType());
+  }
 
 }
--- src/test/java/de/holhar/accounting/report/application/service/deserialization/CreditCardEntryDeserializerTest.java
+++ src/test/java/de/holhar/accounting/report/application/service/deserialization/CreditCardEntryDeserializerTest.java
@@ -3,47 +3,53 @@ package de.holhar.accounting.report.application.service.deserialization;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import de.holhar.accounting.common.MoneyUtils;
+import de.holhar.accounting.report.domain.CreditCardEntry;
+import de.holhar.accounting.report.domain.Entry;
+import de.holhar.accounting.report.domain.EntryType;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 class CreditCardEntryDeserializerTest {
 
-//  @Test
-//  void readStatement() throws IOException {
-//    // Given
-//    CreditCardEntryDeserializer deserializer = new CreditCardEntryDeserializer();
-//    Path path = Paths.get("src/test/resources/accounting/sanitized/cre_202001.csv");
-//    List<String> lines = Files.readAllLines(path);
-//
-//    // When
-//    AccountStatement accountStatement = deserializer.readStatement(lines);
-//
-//    // Then
-//    assertEquals(AccountStatement.Type.CREDIT_CARD, accountStatement.getType());
-//    assertEquals("1111********9999", accountStatement.getId());
-//    assertEquals(LocalDate.parse("2020-01-01"), accountStatement.getFrom());
-//    assertEquals(LocalDate.parse("2020-01-31"), accountStatement.getTo());
-//    assertEquals(new BigDecimal("108.39"), accountStatement.getBalance().getValue());
-//    assertEquals(LocalDate.parse("2020-02-29"), accountStatement.getBalance().getDate());
-//    assertEquals(33, accountStatement.getEntries().size());
-//
-//    CreditCardEntry firstEntry = (CreditCardEntry) accountStatement.getEntries().get(0);
-//    assertTrue(firstEntry.isBilledAndNotIncluded());
-//    assertEquals(LocalDate.parse("2020-02-01"), firstEntry.getValueDate());
-//    assertEquals(LocalDate.parse("2020-01-30"), firstEntry.getReceiptDate());
-//    assertEquals("FOOBAR.COM 7899877898Somewhere", firstEntry.getDescription());
-//    assertEquals(new BigDecimal("-12.72"), firstEntry.getAmount());
-//    assertEquals(new BigDecimal("-14.99"), firstEntry.getOriginalAmount());
-//    assertEquals(EntryType.FOOD_AND_DRUGSTORE, firstEntry.getType());
-//
-//    CreditCardEntry lastEntry = (CreditCardEntry) accountStatement.getEntries().get(32);
-//    assertTrue(lastEntry.isBilledAndNotIncluded());
-//    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getValueDate());
-//    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getReceiptDate());
-//    assertEquals("Einzahlung", lastEntry.getDescription());
-//    assertEquals(new BigDecimal("150.00"), lastEntry.getAmount());
-//    assertEquals(new BigDecimal("0"), lastEntry.getOriginalAmount());
-//    assertEquals(EntryType.INNER_ACCOUNT_TRANSFER, lastEntry.getType());
-//  }
+  @Test
+  void readStatement() throws IOException {
+    // Given
+    CreditCardEntryDeserializer deserializer = new CreditCardEntryDeserializer();
+    Path path = Paths.get("src/test/resources/accounting/sanitized/cre_202001.csv");
+    List<String> lines = Files.readAllLines(path);
+
+    // When
+    Stream<Entry> entryStream = deserializer.readStatement(lines);
+
+    // Then
+    List<Entry> entryList = entryStream.collect(Collectors.toList());
+    assertEquals(33, entryList.size());
+
+    CreditCardEntry firstEntry = (CreditCardEntry) entryList.get(0);
+    assertTrue(firstEntry.isBilledAndNotIncluded());
+    assertEquals(LocalDate.parse("2020-02-01"), firstEntry.getValueDate());
+    assertEquals(LocalDate.parse("2020-01-30"), firstEntry.getReceiptDate());
+    assertEquals("FOOBAR.COM 7899877898Somewhere", firstEntry.getDescription());
+    assertEquals(MoneyUtils.ofMinor(-1272L), firstEntry.getAmount());
+    assertEquals(EntryType.FOOD_AND_DRUGSTORE, firstEntry.getType());
+
+    CreditCardEntry lastEntry = (CreditCardEntry) entryList.get(32);
+    assertTrue(lastEntry.isBilledAndNotIncluded());
+    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getValueDate());
+    assertEquals(LocalDate.parse("2020-01-02"), lastEntry.getReceiptDate());
+    assertEquals("Einzahlung", lastEntry.getDescription());
+    assertEquals(MoneyUtils.ofMinor(15000L), lastEntry.getAmount());
+    assertEquals(EntryType.INNER_ACCOUNT_TRANSFER, lastEntry.getType());
+  }
 }
